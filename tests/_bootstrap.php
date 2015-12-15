30d38b178763b78e8080831ab5cb0e5afdc300ab
Andrii Vasyliev <sol@hiqdev.com>
2015-12-15 12:41:02 +0000

    + tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Generalization over Omnipay and Payum
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
---
+++ tests/unit/HelperTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Cart module for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-cart
+ * @package   yii2-cart
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\tests\unit;
+
+use hiqdev\php\merchant\Helper;
+
+/**
+ * Helper test suite.
+ */
+class HelperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Helper
+     */
+    protected $object;
+
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testId2camel()
+    {
+        $this->assertSame('PostTag', Helper::id2camel('PostTag'));
+        $this->assertSame('PostTag', Helper::id2camel('post-tag'));
+        $this->assertSame('PostTag', Helper::id2camel('post_tag', '_'));
+        $this->assertSame('PostTag', Helper::id2camel('post-tag'));
+        $this->assertSame('PostTag', Helper::id2camel('post_tag', '_'));
+        $this->assertSame('FooYBar', Helper::id2camel('foo-y-bar'));
+        $this->assertSame('FooYBar', Helper::id2camel('foo_y_bar', '_'));
+    }
+
+    public function testSimplify()
+    {
+        $this->assertSame('simpler', Helper::simplify('SimplEr'));
+        $this->assertSame('simpler', Helper::simplify('S im-plEr'));
+    }
+}
