6231d19c9a493beaf38df89faf7f5a94c9de9f5d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-03-23 17:08:09 +0200

    Added WebmoneyMerchant class

---
+++ tests/unit/interkassa/OmnipayMerchantTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * Generalization over Omnipay and Payum
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\tests\unit\interkassa;
+
+use hiqdev\php\merchant\interkassa\OmnipayMerchant;
+
+/**
+ * OmnipayMerchant test suite.
+ */
+class OmnipayMerchantTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var OmnipayMerchant
+     */
+    protected $object;
+
+    protected $data = [
+        'purse'  => 'ThePurse',
+        'secret' => 'TheSecret',
+    ];
+
+    protected function setUp()
+    {
+        $this->object = new OmnipayMerchant();
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testPrepareData()
+    {
+        $res = $this->object->prepareData($this->data);
+        $this->assertSame($this->data['secret'], $res['signKey']);
+    }
+}
