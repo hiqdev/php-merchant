5c8cb6b525570621f636a45144826417e545182d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-28 16:01:10 +0300

    Added yandexmoney adapter

---
+++ src/yandexmoney/OmnipayMerchant.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Generalization over Omnipay and Payum
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\yandexmoney;
+
+/**
+ * Yandex Omnipay Merchant class.
+ */
+class OmnipayMerchant extends \hiqdev\php\merchant\OmnipayMerchant
+{
+    public $requestClass = OmnipayRequest::class;
+
+    public function getWorker()
+    {
+        if ($this->_worker === null) {
+            $this->_worker = (new \yandexmoney\YandexMoney\GatewayIndividual())
+                ->initialize($this->prepareData($this->data));
+        }
+
+        return $this->_worker;
+    }
+
+    public function prepareData(array $data)
+    {
+        return array_merge([
+            'account' => $data['purse'],
+            'password' => $data['secret'],
+        ], $data);
+    }
+}
---
+++ src/yandexmoney/OmnipayRequest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Generalization over Omnipay and Payum
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\yandexmoney;
+
+/**
+ * Yandex Omnipay Merchant class.
+ */
+class OmnipayRequest extends \hiqdev\php\merchant\OmnipayRequest
+{
+    /**
+     * @return \Omnipay\Common\Message\AbstractRequest
+     */
+    public function getWorker()
+    {
+        if ($this->_worker === null) {
+            $this->_worker = $this->merchant->getWorker()->{$this->getType()}($this->getData());
+        }
+
+        return $this->_worker;
+    }
+
+    public function getData()
+    {
+        return array_merge(parent::getData(), [
+            'account' => $this->data['purse'],
+            'form_comment' => $this->data['description'],
+            'orderId' => $this->data['transactionId'],
+        ]);
+    }
+}
