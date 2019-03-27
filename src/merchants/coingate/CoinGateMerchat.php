45108cfaa39216357fed96f7098c52fd0eaad4c9
Yurii Myronchuk <bladeroot@gmail.com>
2019-03-27 14:19:27 +0000

    add CoinGate

---
+++ src/merchants/coingate/CoinGateMerchat.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace hiqdev\php\merchant\merchants\coingate;
+
+use hiqdev\php\merchant\InvoiceInterface;
+use hiqdev\php\merchant\merchants\AbstractMerchant;
+use hiqdev\php\merchant\response\CompletePurchaseResponse;
+use hiqdev\php\merchant\response\RedirectPurchaseResponse;
+use Omnipay\CoinGate\Gateway;
+
+/**
+ * Class CoinGateMerchant.
+ *
+ * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
+ */
+class CoinGateMerchant extends AbstractMerchant
+{
+    /**
+     * @var Gateway
+     */
+    protected $gateway;
+
+    protected function createGateway()
+    {
+        return $this->gatewayFactory->build('CoinGate', [
+            'apiKey'  => $this->credentials->getKey1(),
+            'currency' => $invoice->getCurrency()->getCode(),
+        ]);
+    }
+
+    /**
+     * @param InvoiceInterface $invoice
+     * @return RedirectPurchaseResponse
+     */
+    public function requestPurchase(InvoiceInterface $invoice)
+    {
+        /**
+         * @var \Omnipay\CoinGate\Message\PurchaseResponse $response
+         */
+        $response = $this->gateway->purchase([
+            'transactionId' => $invoice->getId(),
+            'description' => $invoice->getDescription(),
+            'amount' => $this->moneyFormatter->format($invoice->getAmount()),
+            'returnUrl' => $invoice->getReturnUrl(),
+            'cancelUrl' => $invoice->getCancelUrl(),
+        ])->send();
+
+        return new RedirectPurchaseResponse($response->getRedirectUrl(), $response->getRedirectData());
+    }
+
+    /**
+     * @param array $data
+     * @return CompletePurchaseResponse
+     */
+    public function completePurchase($data)
+    {
+        /** @var \Omnipay\CoinGate\Message\CompletePurchaseResponse $response */
+        $response = $this->gateway->completePurchase($data)->send();
+
+        return (new CompletePurchaseResponse())
+            ->setIsSuccessful($response->isSuccessful())
+            ->setAmount($this->moneyParser->parse($response->getAmount(), $response->getCurrency()))
+            ->setTransactionReference($response->getTransactionReference())
+            ->setTransactionId($response->getTransactionId())
+            ->setPayer($response->getPayer())
+            ->setTime(new \DateTime($response->getTime()));
+    }
+}
